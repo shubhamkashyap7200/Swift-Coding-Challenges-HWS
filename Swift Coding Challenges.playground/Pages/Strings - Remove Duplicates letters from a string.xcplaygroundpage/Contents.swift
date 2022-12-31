180ccd467f854979e1187656861d223c34ef6d81
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2022-12-31 23:51:55 +0530

    question - 6 - completed

--- Swift Coding Challenges.playground/Pages/Strings - Remove Duplicates letters from a string.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Strings - Remove Duplicates letters from a string.xcplaygroundpage/Contents.swift
@@ -6,15 +6,43 @@ func spacer() -> () {
 
 // MARK: - Strings - Remove Duplicates letters from a string using for loop
 func removeDuplicateLettersUsingForLoop(_ inputString: String) -> String {
-    var tempCharArray = [Character]()
+    var used = [Character]()
     
     for char in inputString {
-        if tempCharArray.contains(char) {
-            inputString.firstIndex(of: char)
+        if !used.contains(char) {
+            used.append(char)
         }
-        
-        tempCharArray.append(char)
     }
-    return ""
+    return String(used)
 }
 
+print(removeDuplicateLettersUsingForLoop("Hello"))
+spacer()
+
+
+// MARK: - Strings - Remove Duplicates letters from a string using NSOrderedSet
+func removeDuplicateLettersUsingNSOrderedSet(_ inputString: String) -> String {
+    let array = inputString.map{ String($0) }
+    let set = NSOrderedSet(array: array)
+    let letters = Array(set) as! Array<String>
+    return letters.joined()
+}
+
+print(removeDuplicateLettersUsingNSOrderedSet("Nitin"))
+spacer()
+
+// MARK: - Strings - Remove Duplicates letters from a string using Filter Method
+func removeDuplicateLettersUsingFilterMethod(_ inputString: String) -> String {
+    var used = [Character: Bool]()
+    
+    
+    let result = inputString.filter {
+        used.updateValue(true, forKey: $0) == nil
+    }
+    
+    return String(result)
+}
+
+
+print(removeDuplicateLettersUsingFilterMethod("Sara"))
+spacer()
